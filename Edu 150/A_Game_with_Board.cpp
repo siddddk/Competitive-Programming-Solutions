af0285abbcaa95001aa3d7a0f6ed7e7b8a3e0b28
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 14:59:30 +0530

    Upload file via script

---
+++ Edu 150/A_Game_with_Board.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    int n; cin >> n;
+    if(n == 1) {
+        cout << "Alice";
+    }
+    else if(n <= 4) {
+        cout << "Bob";
+    }
+    else {
+        cout << "Alice";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+        cout << '\n';
+    }
+    return 0;
+}
