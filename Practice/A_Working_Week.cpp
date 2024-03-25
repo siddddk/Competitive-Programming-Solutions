db2921ffd7a6015796d5137d92c5dd7f7608fb4e
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:31 +0530

    Upload file via script

---
+++ Practice/A_Working_Week.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+typedef long long ll;
+using namespace std;
+
+
+void solve(void)
+{
+    int n; cin >> n;
+    cout << ((n - 3) / 3) - 1 << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
