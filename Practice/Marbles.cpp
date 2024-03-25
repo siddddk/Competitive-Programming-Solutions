d20c8c75bbebebf02dd441e0b38416c4cb11e77b
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:01:48 +0530

    Upload file via script

---
+++ Practice/Marbles.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+typedef long long ll;
+using namespace std;
+
+
+void solve(void)
+{
+    cout << "Hello World" << "\n";
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
