9ca114e6feaf584e13abb60de3ff3fced59e8cdc
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 14:59:37 +0530

    Upload file via script

---
+++ Practice/pspref.cpp
@@ -0,0 +1,109 @@
+#include "bits/stdc++.h"
+#include <random>
+#include <fstream>
+using namespace std;
+// using namespace __gnu_pbds;
+// template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vvpii = vector<vpii>;
+
+#define Sum(v) (accumulate(v.begin(), v.end(), 0ll))
+#define Sort(v) (sort(v.begin(), v.end()))
+
+
+constexpr ll INF = 1e9;
+constexpr int MOD = 998244353;
+
+// #define IO(NAME) \
+//     cin.tie(0)->sync_with_stdio(0); \
+//     if(fopen(NAME ".in","r")) freopen(NAME ".in","r",stdin), \
+//     freopen(NAME ".out","w",stdout);
+
+template <typename T1, typename T2> // cin >> pair<T1, T2>
+istream &operator>>(istream &istream, pair<T1, T2> &p)
+{
+    return (istream >> p.first >> p.second);
+}
+ 
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+ 
+template <typename T1, typename T2> // cout << pair<T1, T2>
+ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+
+// bitwise ops
+// j is 0 based indexing 
+// _builtin_ctzll() for long long and _ builtin_popcount()
+#define LSB(S) ((S) & -(S))
+
+
+ll LCM(int a, int b)
+{
+    return ((ll)a * b) / __gcd(a, b);
+}
+
+bool nCr_Parity(int n, int r){
+    int i = 0;
+    while(i <= 30){ // for 32 bit integers
+        int x = 1<<i;
+        if((x&r) > (x&n)) return 0;
+        ++i;
+    }
+    return 1; // 1 is odd 
+}
+
+void solve(){
+    random_device rd; 
+    mt19937 gen(rd()); 
+    uniform_int_distribution<> distr(1, 2);
+    cout << 100000 << " " << 1 << "\n";
+    for(int i = 0; i < 100000; i++) {
+        for(int j = 0; j < 1; j++) {
+            cout << distr(gen) << " ";
+        }
+        cout << "\n";
+    }
+
+    uniform_int_distribution<> distrq(1, 100000);
+    cout << 100000 << "\n";
+    for(int i = 0; i < 100000; i++) {
+        int a = distrq(gen), b = distrq(gen);
+        if(a > b) swap(a, b);
+        cout << a << " " << b << "\n";
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << setprecision(10) << fixed;
+    // IO(""); 
+
+    solve();
+}
+// when using printf, scanf for long long use %dll
+// when using accumulate use 0ll if sum is long long
