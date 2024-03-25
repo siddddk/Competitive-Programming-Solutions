16dcc98cb316c7ba237cf0825b1fc353bcb2af4f
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:45 +0530

    Upload file via script

---
+++ Practice/B_Stand-up_Comedian.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (ll) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    vector<ll> a(4); cin >> a;
+    if(a[0] == 0) {
+        cout << 1 << "\n";
+    }
+    else {
+        cout << (a[0] + 2*min(a[1], a[2]) + min(a[0] + 1, abs(a[1] - a[2]) + a[3])) << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
