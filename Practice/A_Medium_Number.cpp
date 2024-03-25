a4b331cc7beb69668e39d70d71c047a7ca80aec9
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:27 +0530

    Upload file via script

---
+++ Practice/A_Medium_Number.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+typedef long long ll;
+using namespace std;
+
+
+void solve(void)
+{
+    ll a[3];
+    cin >> a[0] >> a[1] >> a[2];
+
+    sort(a, a + 3);
+    cout << a[1] << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
