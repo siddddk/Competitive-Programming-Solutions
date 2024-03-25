9a65b359e1fa9e10cd395ace209d18443e61418d
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:33 +0530

    Upload file via script

---
+++ Practice/B_Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+typedef long long ll;
+using namespace std;
+
+
+void solve(void)
+{
+    int n; cin >> n;
+    string s; cin >> s;
+    sort(s.begin(), s.end());
+    cout << s[n - 1] - 'a' + 1 << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
