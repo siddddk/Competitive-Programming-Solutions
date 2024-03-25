1719e569f8b085a910ae9c9abf151ae44e39056b
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 14:59:27 +0530

    Upload file via script

---
+++ Edu 149/A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    int x, k; cin >> x >> k;
+    if(x == 0) {
+        cout << 0 << '\n';
+    }
+    else if(x % k) {
+        cout << 1 << '\n';
+        cout << x << '\n';
+    }
+    else {
+        cout << 2 << '\n';
+        cout << x - 1 << " " << 1 << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
