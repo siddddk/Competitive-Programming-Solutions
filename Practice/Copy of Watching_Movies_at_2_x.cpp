14de92fefd0ff9f9806533073aaf65a26c8a7823
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:53 +0530

    Upload file via script

---
+++ Practice/Copy of Watching_Movies_at_2_x.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll x, y;
+    cin >> x >> y;
+
+    cout << (x - y/2) << "\n";
+    return 0;
+}
