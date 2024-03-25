e7bcf3e098841155acb13e4ac3d96cfa55884c18
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:34 +0530

    Upload file via script

---
+++ Practice/B_Cake_Assembly_Line.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
