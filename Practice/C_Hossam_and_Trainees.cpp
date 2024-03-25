4578555909574cefacddd0b4292d29f880905458
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:01:02 +0530

    Upload file via script

---
+++ Practice/C_Hossam_and_Trainees.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (ll) 1e9 + 7
+typedef long long ll;
+using namespace std;
+
+void solve(void)
+{
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
