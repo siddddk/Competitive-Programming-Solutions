b0c3523cc1be2d67dd65201944f80d679364364c
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:26 +0530

    Upload file via script

---
+++ Practice/A_Madoka_and_Strange_Thoughts.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+typedef long long ll;
+using namespace std;
+
+
+void solve(void)
+{
+    ll n;
+    cin >> n;
+    cout << n << " WORKS";
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll T;
+    int* arr = new int[100000000]; 
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
