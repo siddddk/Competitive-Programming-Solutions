7b7ee77a2d6354c1b621b6fd0a62c5f2c4afe5f5
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:29 +0530

    Upload file via script

---
+++ Practice/A_Prefix_and_Suffix_Array.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    int n; cin >> n;
+    vector<string> v;
+    for(int i = 0; i < 2 * n - 2; i++) {
+        string s; cin >> s;
+        if(s.length() == n - 1) v.PB(s);        
+    }
+    reverse(all(v[1]));
+    if(v[0] == v[1]) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
