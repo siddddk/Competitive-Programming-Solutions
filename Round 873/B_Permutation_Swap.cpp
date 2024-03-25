476d532f4f8a79beb91c5d9d5a8da0011a2549eb
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:02 +0530

    Upload file via script

---
+++ Round 873/B_Permutation_Swap.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    int n; cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        a[i]--;
+    }
+
+    int ans = abs(a[0]);
+    for(int i = 0; i < n; i++) {
+        ans = __gcd(ans, abs(a[i] - i));
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
