dba8f924acff776f8852cd8005547421b0a1e481
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 15:00:00 +0530

    Upload file via script

---
+++ Round 872/A_LuoTianyi_and_the_Palindrome_String.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    string s; cin >> s;
+    char c = s[0];
+    bool flag = false;
+    for(int i = 1; i < s.length(); i++) {
+        if(s[i] != c) {
+            flag = true;
+            break;
+        }
+    }   
+    if(flag) {
+        cout << s.length() - 1 << "\n";
+    }
+    else cout << -1 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
