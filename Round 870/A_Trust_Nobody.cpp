bff97f9e0c750ae5bafc5e5d03a7e9af5db0afc8
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 14:59:58 +0530

    Upload file via script

---
+++ Round 870/A_Trust_Nobody.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    int n; cin >> n;
+    vector<int> l(n); cin >> l;
+    sort(all(l));
+    for(int i = 0; i < n; i++) {
+        int truth = 0;
+        for(int j = 0; j < n; j++) {
+            if(l[j] <= i) truth++;
+        }
+        if(truth + i == n) {
+            cout << i << "\n";
+            return;
+        }
+    }       
+    cout << -1 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
