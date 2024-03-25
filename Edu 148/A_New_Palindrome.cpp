8d92884ce9123a616952b2e9d338542eabeebe3c
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 14:59:26 +0530

    Upload file via script

---
+++ Edu 148/A_New_Palindrome.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    string s; cin >> s;
+    int i = (s.length() + 1) / 2;
+    char c = s[i];
+    ++i;
+    bool flag = false;
+    for( ;i < s.length(); i++) {
+        if(s[i] != c) {
+            flag = true;
+        }
+    }
+    if(flag) {
+        cout << "YES\n";
+    }
+    else cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
