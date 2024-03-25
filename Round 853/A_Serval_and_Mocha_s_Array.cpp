0ddce17796f5a3365107cb71152ce6b1db854e23
SiddhantKulkarni03 <101969791+SiddhantKulkarni03@users.noreply.github.com>
2024-03-25 14:59:55 +0530

    Upload file via script

---
+++ Round 853/A_Serval_and_Mocha_s_Array.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MOD (int) 1e9 + 7
+#define all(x) begin(x), end(x)
+typedef long long ll;
+using namespace std;
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve(void) 
+{
+    int n; cin >> n;
+    vector<int> a(n); cin >> a;
+    sort(all(a));
+    int allgcd = a[0];
+    bool flag = false;
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(__gcd(a[i], a[j]) <= 2) flag = true;
+        }
+    }
+    if(flag) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
